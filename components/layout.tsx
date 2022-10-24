2fa60499673f165f65941283854366443ac55088
NIL-99 <mingyong1999@gmail.com>
2022-10-24 23:37:21 +0800

    Fixing syntax error with Layout.tsx

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,18 +1,15 @@
 import Head from "next/head";
 
-export const siteTitle = "MingYong"
+export const siteTitle = "MingYong";
 
-export default function Layout(children: React.ReactNode; ) {
-    return (
-        <div>
-            <Head>
-                <link ref="icon" href="/../public/my-icon.png" />
-				<meta
-					name="description"
-					content="Ming Yong portfolio"
-				/>
+export default function Layout(children: React.ReactNode) {
+	return (
+		<div>
+			<Head>
+				<link ref="icon" href="/../public/my-icon.png" />
+				<meta name="description" content="Ming Yong portfolio" />
 				<meta name="og:title" content={siteTitle} />
-            </Head>
-        </div>
-    )
+			</Head>
+		</div>
+	);
 }
